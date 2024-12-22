f70aad3da65bd9521eccd99989821f9f69a73a4b
[Abi-Vike] <[tomy0abr@gmail.com]>
2024-12-22 08:33:26 +0100

    first iteration of firebase deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+A Responsive Free One Page Portfolio/Blog Website for Dave Solo
+
+By Abiel - Originally from themewagon.com
---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTNl2hAVfccqORyNKI0J1Zrc1cH-QObzw",
+  authDomain: "snit-s-blog.firebaseapp.com",
+  projectId: "snit-s-blog",
+  storageBucket: "snit-s-blog.firebasestorage.app",
+  messagingSenderId: "43868096657",
+  appId: "1:43868096657:web:a4b621feec6a5671e0d730"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
